a9c9717cbfbfa2c4e81ed10389e771637d0ba92d
Nick Kramer <krames12@gmail.com>
2018-06-01 08:00:59 -0400

    description blurb re-worked, projects coming soon, blog list styled

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,16 +3,21 @@ import Link from 'gatsby-link'
 
 const BlogList = ({data}) => (
   <div>
-    <h1>Ramblings And Thoughts</h1>
+    <h2>Ramblings And Thoughts</h2>
 
-    <ul>
-        {data.allMarkdownRemark.edges.map(({node}) => (
-            <li>
+    <ul id="blog-post-list">
+        {data.allMarkdownRemark.edges.map( ({node}) => (
+            <li className="blog-post-card">
                 <Link
                     key={node.id}
-                    to={node.frontmatter.path}>
+                    to={node.frontmatter.path}
+                    className="blog-post-title">
                     {node.frontmatter.title}
                 </Link>
+                <div className="blog-post-card-contents">
+                    <p className="blog-post-date">{node.frontmatter.date}</p>
+                    <p className="blog-post-excerpt">{node.excerpt}</p>
+                </div>
             </li>
         ) )}
     </ul>
@@ -22,11 +27,16 @@ const BlogList = ({data}) => (
 
 export const listQuery = graphql`
     query BlogListQuery {
-        allMarkdownRemark(limit: 10) {
+        allMarkdownRemark(
+            limit: 10
+            sort: { fields: [frontmatter___date], order: DESC }
+        ) {
             edges {
               node {
                 id
+                excerpt(pruneLength: 300)
                 frontmatter {
+                date(formatString: "MMMM DD, YYYY")
                   path
                   title
                 }
