6cff58d597c95909b2d40e23daa5c958b4f50709
Nick Kramer <krames12@gmail.com>
2018-12-18 07:28:07 -0500

    created json store of project info to pass to project components

---
+++ src/project-list.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    title: 'PugCheck Teams',
+    image: 'Character-View-PugCheck.png',
+    technologies: ['Laravel', 'PHP', 'MySQL', 'SCSS'],
+    links: [
+      {
+        url: 'https://teams.pugcheck.com',
+        classes: 'icon fas fa-link',
+      },
+      {
+        url: 'https://github.com/krames12/pugcheck-teams',
+        classes: 'icon fab fa-github',
+      },
+    ],
+    description: `Supplying World of Warcraft raid teams with a place to organize teammates in their assigned roles as well as pulling important character information from Blizzard’s API.`,
+  },
+  {
+    title: 'PugCheck',
+    image: 'Character-View-PugCheck.png',
+    technologies: ['Laravel', 'PHP', 'MySQL', 'SCSS'],
+    links: [
+      {
+        url: 'https://pugcheck.com/us/proudmoore/boombawks',
+        classes: 'icon fas fa-link',
+      },
+      {
+        url: 'https://github.com/krames12/pugcheck',
+        classes: 'icon fab fa-github',
+      },
+    ],
+    description: `A World of Warcraft character progression lookup tool that allows users to check raid progression and logs to gauge player performance. Using the{' '}
+    <a href="www.worldofwarcraft.com">World of Warcraft</a> &{' '}
+    <a href="www.warcraftlogs.com">WarcraftLogs</a> API's.`,
+  },
+]
